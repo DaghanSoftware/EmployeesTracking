7dfb1c9a2c94a19ef99e140a2ddf87fe887f0a50
Semih Dağhan <daghanex@hotmail.com>
2022-06-13 17:51:07 +0300

    Listeleme işlemlerinde revizeler yapıldı
    Artık verileri datatable üzerinden değil partial view üzerinden listeliyorum

--- EmployeesTracking/Controllers/DenemeController.cs
+++ EmployeesTracking/Controllers/DenemeController.cs
@@ -78,6 +78,7 @@ namespace EmployeesTracking.Controllers
             return Json(jsonWriters);
 
         }
+   
         public IActionResult AjaxileVeriSilme(int id)
         {
             _context.Personels.Remove(_context.Personels.SingleOrDefault(e => e.Id == id));
@@ -90,9 +91,41 @@ namespace EmployeesTracking.Controllers
             //var jsonWriters = JsonConvert.SerializeObject(employee);
             //return Json(jsonWriters);
         }
-        public IActionResult PersonelListesiPartial(int id)
+        public IActionResult PersonelListesiPartial(string q, int gendernumber, int maritalnumber, int sehir, int page = 1)
         {
-            return View();
+            var query = (from p in _context.Personels
+                         join s in _context.Genders on p.GenderId equals s.GenderId
+                         join c in _context.Cities on p.CityId equals c.CityId
+                         join m in _context.MaritalStatus on p.MaritalStatusId equals m.MaritalStatusId
+                         //where p.GenderId==gendernumber || p.MaritalStatusId==maritalnumber
+                         select new PersonelViewModel
+                         {
+                             Id = p.Id,
+                             Adi = p.Adi,
+                             Soyadi = p.Soyadi,
+                             TcNo = p.TcNo,
+                             BabaAdi = p.BabaAdi,
+                             AnaAdi = p.AnaAdi,
+                             GenderId = p.GenderId,
+                             GenderName = s.GenderName,
+                             MaritalStatusId = p.MaritalStatusId,
+                             MaritalStatusName = m.MaritalStatusName,
+                             CityId = p.CityId,
+                             CityName = c.CityName
+                         });
+
+            if (gendernumber > 0)
+                query = query.Where(x => x.GenderId == gendernumber);
+            if (maritalnumber > 0)
+                query = query.Where(x => x.MaritalStatusId == maritalnumber);
+            if (sehir > 0)
+                query = query.Where(x => x.CityId == sehir);
+            if (!string.IsNullOrEmpty(q))
+            {
+                query = query.Where(i => i.Adi.ToLower().Contains(q.ToLower()) || i.Soyadi.ToLower().Contains(q.ToLower()));
+            }
+            var yazarlar = query.ToList();
+            return View(yazarlar);
         }
         public static List<EmployeeList> employee = new List<EmployeeList>
         {
