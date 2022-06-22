230a0c7dc3de383e601592e6955f174d5f5c130d
Semih Dağhan <daghanex@hotmail.com>
2022-06-22 23:40:50 +0300

    Jquery Validate
    Jquery Validate ile fluent validation anlık olarak error message listeleme işleminin denemelerini yaptım.

---
+++ EmployeesTracking/Controllers/FluentController.cs
@@ -0,0 +1,27 @@
+﻿using EmployeesTracking.Entities;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EmployeesTracking.Controllers
+{
+    public class FluentController : Controller
+    {
+        public IActionResult FluentDeneme()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult FluentDeneme(Admin model)
+        {
+            if (!ModelState.IsValid)
+            {
+                var messages = ModelState.ToList();
+                return View(model);
+            }
+            return View();
+        }
+    }
+}
